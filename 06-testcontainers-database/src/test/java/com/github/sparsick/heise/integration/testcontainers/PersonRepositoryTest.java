e684a16fb60e91ed321ca6afdfdc6f8432f2a0f0
Sandra Parsick <mail@sandra-parsick.de>
2023-10-18 08:25:14 +0200

    final database test

--- 06-testcontainers-database/src/test/java/com/github/sparsick/heise/integration/testcontainers/PersonRepositoryTest.java
+++ 06-testcontainers-database/src/test/java/com/github/sparsick/heise/integration/testcontainers/PersonRepositoryTest.java
@@ -1,4 +1,50 @@
 package com.github.sparsick.heise.integration.testcontainers;
 
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.ext.ScriptUtils;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
 public class PersonRepositoryTest {
+
+    @Container
+    private PostgreSQLContainer database = new PostgreSQLContainer(DockerImageName.parse("postgres"));
+    private PersonRepository repositoryUnderTest;
+
+    @BeforeEach
+    void setup(){
+        ScriptUtils.runInitScript(new JdbcDatabaseDelegate(database, ""), "database/init.sql");
+        HikariConfig configuration = new HikariConfig();
+        configuration.setJdbcUrl(database.getJdbcUrl());
+        configuration.setUsername(database.getUsername());
+        configuration.setPassword(database.getPassword());
+        DataSource dataSource = new HikariDataSource(configuration);
+        repositoryUnderTest = new PersonRepository(dataSource);
+    }
+
+    @Test
+    void aroundTrip() {
+        var person = new Person();
+        person.setFirstName("Alice");
+        person.setLastName("Bob");
+        person.setJobTitle("Dev");
+        repositoryUnderTest.save(person);
+
+        List<Person> allPersons = repositoryUnderTest.findAllPersons();
+
+        assertTrue(allPersons.size() == 1);
+    }
 }
